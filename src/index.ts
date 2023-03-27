43b87731b3698ebc9e5d4bf24831f8b598d2a602
alexis mouchon <alexismouchon09@gmail.com>
2023-03-27 16:52:38 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import process from "process";
+import dotenv from "dotenv-flow";
+import Discord from "discord.js";
+
+dotenv.config({
+  default_node_env: "development",
+  silent: true,
+});
+
+const main = async () => {
+  const client = new Discord.Client({
+    intents: [
+      Discord.GatewayIntentBits.Guilds,
+      Discord.GatewayIntentBits.GuildMessages,
+      Discord.GatewayIntentBits.GuildMessageReactions,
+      Discord.GatewayIntentBits.MessageContent,
+    ],
+  });
+  const token = process.env.DISCORD_TOKEN;
+
+  client.login(token);
+
+  client.on("ready", () => {
+    console.log(`Logged in as ${client.user?.tag}!`);
+  });
+};
+
+main();
